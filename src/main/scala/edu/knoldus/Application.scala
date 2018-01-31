e6d0659c82974d4ac7c436b72ee29303c4d06454
Randhir <randhir.kumar@knoldus.in>
2018-01-31 11:09:39 +0530

    Futute implementation done

--- src/main/scala/edu/knoldus/Application.scala
+++ src/main/scala/edu/knoldus/Application.scala
@@ -3,15 +3,20 @@ package edu.knoldus
 import java.io.File
 
 import scala.io.StdIn._
+
 import org.apache.log4j.Logger
 
 object Application extends App {
 
   val Printer = Logger.getLogger(this.getClass)
-  Printer.info(" enter folder path  ")
-  val FolderPath = readLine()
-  val Folder = new File(FolderPath).listFiles().toList
-  val FilePath = new FilePath().getFilePath(Folder)
-  Printer.info(FilePath)
-  Thread.`yield`()
+
+  try {
+    Printer.info(" enter folder path  ")
+    val folderPath = readLine()
+    val filePath = new FilePath().getFilePath(new File(folderPath).listFiles().toList)
+    Printer.info(filePath)
+  }
+  catch {
+    case ex => Printer.info(ex)
+  }
 }
--- src/main/scala/edu/knoldus/FilePath.scala
+++ src/main/scala/edu/knoldus/FilePath.scala
@@ -1,15 +1,19 @@
 package edu.knoldus
 
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
 import java.io.File
 
 class FilePath {
 
-  def getFilePath(file: List[File], filePaths: List[File] = Nil): List[File] = {
+  def getFilePath(file: List[File], filePaths: List[File] = Nil): Future[List[File]] = {
 
     file match {
-      case first :: remain if first.isDirectory => getFilePath(first.listFiles().toList ++ remain, filePaths)
+      case Nil => Future { filePaths }
+      case first :: remain if first.isDirectory => getFilePath(remain ++ first.listFiles().toList, filePaths)
       case first :: remain if first.isFile => getFilePath(remain, filePaths :+ first)
-      case first :: Nil if first.isFile => getFilePath(Nil, filePaths :+ first)
+      case _ => Future {  Nil }
     }
+
   }
 }
